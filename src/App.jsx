9d06b1819b2b372c46073914240aecacc6a97dd2
Shubham Ranpura <ranpuradipti02@gmail.com>
2025-06-09 16:48:16 +0530

    final Changes

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import NestedBox from './Componannt/NeastedBox'; 
+
+function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const url = window.location.href;
+    const number = url.match(/\d+/g);
+    
+    const boxNumber = Number(number[1])
+    if(!isNaN(boxNumber)){setValue(boxNumber)}
+    else{
+      window.alert("Enter numbet in url")
+    }
+  }, []);
+
+  return (
+    <div className='w-[100%] h-[110vh] flex justify-center items-center '>
+      <NestedBox value={value} />
+    </div>
+  );
+}
+
+export default App;
